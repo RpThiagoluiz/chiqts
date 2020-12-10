f2cf11b96aee1f3fd2cb84b3f5a705c2b3da4c86
driel <rp.thiagoluiz@gmail.com>
2020-12-10 09:44:08 -0300

    testPComp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react'
 
 //Components
 import Title from './components/Card'
+import P from './components/P'
 
 //styles
 import './globaltheme.css'
 
 const App = () => (
- <Title/>
-)
+    <>
+      <Title/>
+      <P />
+    </>
+  )
+
+ 
+
 
 export default App
---
+++ src/components/P/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const P = () => (
+    <small>test alfa text small P!</small>
+)
+
+export default P
